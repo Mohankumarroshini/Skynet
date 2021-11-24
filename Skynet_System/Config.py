b2f2146982f83910338e53785170d59a40f5c46a
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-24 20:23:08 +0530

    Update Config.py

--- Skynet_System/Config.py
+++ Skynet_System/Config.py
@@ -7,5 +7,32 @@ Skynet_logs: Union[str, int] = 123456789
 Skynet_approved_logs: Union[str, int] = 123456789
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
+telethon
+
+aiohttp
+
+motor 
+
+heroku3
+
+pillow
+
+dnspython
+
+MONGI_DB_URL
+
+telethon
+
+aiohttp
+
+motor 
+
+heroku3
+
+pillow
+
+dnspython
+
+MON
 
 # Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
