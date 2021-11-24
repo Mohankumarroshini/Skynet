d7eee6e0aa909c7471996a38b5247b70036ed6bb
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-24 19:38:50 +0530

    Create config.py

---
+++ Skynet_System/config.py
@@ -0,0 +1,19 @@
+import Skynet_Scanner.config as Config
+
+  # REQUIRED
+
+    API_ID_KEY = "draj is op"
+
+    API_HASH_KEY = "lol dal isee "  
+
+    STRING_SESSION = "ssyour"
+
+    Skynet = ""
+
+    ENFORCERS = ""
+
+    Skynet_logs = ""
+
+    Skynet_approved_logs = ""
+
+    GBAN_MSG_LOGS = ""
